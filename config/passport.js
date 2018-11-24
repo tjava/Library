8416a52cf83d1c6b74e040642c4f1e2ecaccd340
Tjava <mustyytee@gmail.com>
2018-11-23 16:33:26 -0800

    stage 1

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var secret = require('../config/secret');
+var async = require('async');
+var request = require('request');
 
 var User = require('../models/user');
 
@@ -16,6 +18,7 @@ passport.deserializeUser(function(id, done) {
 });
 
 
+
 passport.use(new FacebookStrategy(secret.facebook, function(req, token, refreshToken, profile, done) {
   User.findOne({ facebook: profile.id }, function(err, user) {
     if (err) return done(err);
@@ -25,6 +28,8 @@ passport.use(new FacebookStrategy(secret.facebook, function(req, token, refreshT
       return done(null, user);
     } else {
 
+      async.waterfall([
+        function(callback) {
           var newUser = new User();
           newUser.email = profile._json.email;
           newUser.facebook = profile.id;
@@ -34,11 +39,36 @@ passport.use(new FacebookStrategy(secret.facebook, function(req, token, refreshT
 
           newUser.save(function(err) {
             if (err) throw err;
-            req.flash('loginMessage', 'Successfully login with facebook');
-            return done(null, newUser)
+            req.flash('loginMessage', 'Successfully sign up with facebook');
+            callback(err, newUser)
           });
+        },
 
-     
+        function(newUser, callback) {
+          //Mailchimp request
+          request({
+            url: 'https://us19.api.mailchimp.com/3.0/lists/2068255d5b/members',
+            method: 'POST',
+            headers: {
+              'Authorization': 'randomUser e3932f00d4ab31656e3c9d7e995e2e02-us19',
+              'Content-Type': 'application/json'
+            },
+            json: {
+              'email_address': newUser.email,
+              'status': 'subscribed'
+            }
+
+          }, function(err, response, body) {
+            //Do something here
+            if (err) {
+              return done(err, newUser);
+            } else {
+              console.log("Success");
+              return done(null, newUser);
+            }
+          });
+        }
+      ]);
 
     }
   });
